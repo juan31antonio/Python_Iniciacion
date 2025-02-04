86647d8d4ac2c5abbdfd625bb9852cad4d49f930
juan31antonio <juanantoniocorder32@gmail.com>
2025-02-04 13:45:18 +0100

    Empezando python 1

---
+++ EjerciciosRepaso/repaso.py
@@ -0,0 +1,21 @@
+#Ejercicio 1:
+
+def funcionRecibe(parrafo):
+    lista = parrafo.split()
+    diccionario = {}
+    for palabra in lista:
+        if palabra in dict:
+            diccionario[palabra] += 1
+        else:
+            diccionario[palabra] = 1
+
+funcionRecibe("Hola que yal estas")
+#Ejercicio 2:
+#Ejercicio 3:
+#Ejercicio 4:
+#Ejercicio 5:
+#Ejercicio 6:
+#Ejercicio 7:
+#Ejercicio 8:
+#Ejercicio 9:
+#Ejercicio 10:
